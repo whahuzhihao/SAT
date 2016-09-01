f01327f0bd93b4fc6bd335fe004b72b2129bc284
胡搞 <whahuzhihao@126.com;whahuzhihao@gmail.com;whahuzhihao@hotmail.com>
2016-09-01 15:43:02 +0800

    remove

--- src/Entity/Response.php
+++ src/Entity/Response.php
@@ -37,17 +37,4 @@ class Response {
         $this->overlap = INF;
         return $this;
     }
-
-    /**
-     * Returns a new Polygon whose edges are the edges of the box.
-     * @return Polygon
-     */
-    public function toPolygon(){
-        return new Polygon(new Vector($this->pos->x, $this->pos->y),
-            array(
-                new Vector(), new Vector($this->w, 0),
-                new Vector($this->w,$this->h), new Vector(0,$this->h)
-            )
-        );
-    }
 }
